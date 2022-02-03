1b50ffb7fb1e1dd7f1665157d3cdd55134607470
bolioliagustin <bolioliagustin@gmail.com>
2022-02-03 19:40:35 -0300

    s

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,11 +19,13 @@ if __name__ == '__main__':
     todos = requests.get('https://jsonplaceholder.typicode.com/todos',
                          params={'userId': user_id}, verify=False).json()
     user_name = user.get('username')
-    tasks = []
-    for task in todos:
-        tasks.append({'task': task.get('title'),
-                      'completed': task.get('completed'),
-                      'username': user_name})
+    tasks = [task for task in todos]
+    dict1 = {argv[1]: []}
+    for task in tasks:
+        dict2 = {'task': task.get('title'),
+                 'completed': task.get('completed'),
+                 'username': user_name}
+        dict1[argv[1]].append(dict2)
     with open('{}.json'.format(user_id), 'w') as json_file:
-        json.dump({user_id: tasks}, json_file)
+        json.dump(dict1, json_file)
     json_file.close()
